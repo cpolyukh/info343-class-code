e4fa7cedf50b81e67a751edf453084b2d5b0418b
Christina Polyukh <cpolyukh@uw.edu>
2015-10-22 16:46:56 -0700

    class work for parse

--- parse/js/app.js
+++ parse/js/app.js
@@ -12,6 +12,7 @@ $(function() {
     //new query that will return all tasks ordered by createAt
     var tasksQuery = new Parse.Query(Task);
     tasksQuery.ascending('createdAt');
+    tasksQuery.notEqualTo('done', true);
 
     //reference to the task list element
     var tasksList = $('#tasks-list');
@@ -21,6 +22,8 @@ $(function() {
 
     //current set of tasks
     var tasks = [];
+
+    var ratingElem = $('#rating');
 
     function displayError(err) {
         errorMessage.text(err.message);
@@ -39,13 +42,6 @@ $(function() {
         $('.fa-spin').hide();
     }
 
-    function fetchTasks() {
-        showSpinner();
-        tasksQuery.find()
-            .then(onData, displayError)
-            .always(hideSpinner);
-    }
-
     function onData(results) {
         tasks = results;
         renderTasks();
@@ -54,29 +50,75 @@ $(function() {
     function renderTasks() {
         tasksList.empty();
         tasks.forEach(function(task) {
-            $(document.createElement('li'))
+            var li = $(document.createElement('li'))
                 .text(task.get('title'))
-                .appendTo(tasksList);
+                .addClass(task.get('done') ? 'completed-task' : '')
+                .appendTo(tasksList)
+                .click(function() {
+                    task.set('done', !task.get('done'));
+                    task.save().then(renderTasks, displayError);
+                });
+
+            $(document.createElement('span'))
+                .raty({readOnly: true,
+                    score: (task.get('rating') || 1),
+                    hints: ['crap', 'awful', 'ok', 'nice', 'awesome']})
+                .appendTo(li);
         })
+    }
+
+    function showMessage(message) {
+        message = message || 'Hello';
+        alert(message);
+    }
+
+    showMessage('World');
+
+    function fetchTasks() {
+        showSpinner();
+        tasksQuery.find()
+            .then(onData, displayError)
+            .always(hideSpinner);
     }
 
     //when the user submits the new task form...
     $('#new-task-form').submit(function(evt) {
+        //tell the browser not to do its default behavior
         evt.preventDefault();
 
+        //find the input element in this form
+        //with a name attribute set to "title"
         var titleInput = $(this).find('[name="title"]');
+
+        //get the current value
         var title = titleInput.val();
+
+        //create a new Task and set the title
         var task = new Task();
         task.set('title', title);
-        task.save().then(fetchTasks, displayError).then(function() {
-            titleInput.val('');
-        });
+        task.set('rating', ratingElem.raty('score'));
 
+        //save the new task to your Parse database
+        //if save is successful, fetch the tasks again
+        //otherwisedisplay the error
+        //regardlesss, clear the title title input
+        //so the user can enter the next new task
+        task.save()
+            .then(fetchTasks, displayError)
+            .then(function() {
+                titleInput.val('');
+                ratingElem.raty('set', {});
+            });
+
+        //some browsers also require that we return false to
+        //prevent the default behavior
         return false;
     });
 
     //go and fetch tasks from Parse
     fetchTasks();
+
+    ratingElem.raty();
 
     window.setInterval(fetchTasks, 3000);
 });
